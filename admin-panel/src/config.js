29585ec05fa91bf1703a85802b3a317dd5035cc3
pranahu Kumar <pranshukumar9519@gmail.com>
2025-11-23 22:46:17 +0530

    Fixed Port Binding for Render

--- admin-panel/src/config.js
+++ admin-panel/src/config.js
@@ -3,8 +3,8 @@
 // ============================================
 
 // Jab tak Render ka URL nahi milta, Localhost use karein
-export const API_BASE_URL = "http://localhost:3000/api";
+//export const API_BASE_URL = "http://localhost:3000/api";
 
 // Jab Backend LIVE ho jaye, to upar wali line ko comment karein
 // aur niche wali line mein apna Render URL daal kar uncomment karein:
-// export const API_BASE_URL = "https://sewaone-api.onrender.com/api";
+export const API_BASE_URL = "https://sewaone-api.onrender.com/api";
